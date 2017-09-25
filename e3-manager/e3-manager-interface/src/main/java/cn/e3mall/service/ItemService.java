e7f0df5c3cf4d3435518602bbd81ddf6dc2b7627
石泽轩 <shameier521@163.com>
2017-09-25 11:29:34 +0800

    添加manager工程

---
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -0,0 +1,15 @@
+package cn.e3mall.service;
+
+import cn.e3mall.pojo.TbItem;
+
+/**
+	生活不止眼前的苟且，
+			还有远方的诗和姐~~~
+									@眉黛凶兆
+									2017年9月25日上午9:41:46
+*/
+
+public interface ItemService {
+	
+	TbItem getItemById(long itemId);
+}
---
+++ e3-manager/e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,35 @@
+package cn.e3mall.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+
+/**
+	生活不止眼前的苟且，
+			还有远方的诗和姐~~~
+									@眉黛凶兆
+									2017年9月25日上午9:50:12
+*/
+/**
+ * 商品管理Controller
+ * @author szx
+ *
+ */
+@Controller
+public class ItemController {
+	
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable long itemId){
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+	}
+}
